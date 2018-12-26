9df0c0d0b84849d7251baf72831367d8482d1d2c
Trevor Clinkenbeard <trevorclinkenbeard@gmail.com>
2018-12-25 22:45:04 -0600

    Mapped jj to <ESC> in insert mode

--- init.el
+++ init.el
@@ -135,5 +135,7 @@
        ;; the defaults module. It contains a Spacemacs-inspired keybinding
        ;; scheme and additional ex commands for evil-mode. Use it as a reference
        ;; for your own.
-       :private default)
+       :private
+       default
+       key-chord)
 
---
+++ modules/private/key-chord/config.el
@@ -0,0 +1,5 @@
+(def-package! key-chord)
+
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
